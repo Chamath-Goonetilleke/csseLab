7ce99decaa528fc7da755ea63c1ac053511db5c5
Kavindu Madushan <kavindu@tsdclanka.com>
2022-10-24 03:44:14 +0530

    implement Db connection with singleton patterns

--- src/com/employeemanagement/commons/CommonUtil.java
+++ src/com/employeemanagement/commons/CommonUtil.java
@@ -9,15 +9,12 @@ import java.util.Properties;
 
 public class CommonUtil {
 
-<<<<<<< HEAD:src/com/employeemanagement/commons/c1.java
+
 	//create Properties
 	public static final Properties property = new Properties();
 	//initialize Logger
-	public static final Logger LOG = Logger.getLogger(c1.class.getName());
-=======
-	public static final Properties p = new Properties();
 	public static final Logger LOG = Logger.getLogger(CommonUtil.class.getName());
->>>>>>> 7f07785135313f59ceb3e0e5f3dec17976a1f067:src/com/employeemanagement/commons/CommonUtil.java
+
 
 	static {
 		try {
